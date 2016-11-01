461975a5060cea32fb40c82f916f1481802e7fca
Tyrone Saunders <tyronemsaunders@users.noreply.github.com>
2016-11-01 11:19:53 -0500

    Update Zookeeper dependency to 8.0 and change version.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'EverTrue'
 maintainer_email 'devops@evertrue.com'
 license          'Apache v2.0'
 description      'Installs Netflix Exhibitor'
-version          '3.0.0'
+version          '4.0.0'
 
 issues_url 'https://github.com/evertrue/exhibitor-cookbook/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/evertrue/exhibitor-cookbook/' if respond_to?(:source_url)
@@ -11,7 +11,7 @@ source_url 'https://github.com/evertrue/exhibitor-cookbook/' if respond_to?(:sou
 depends          'build-essential'
 depends          'java', '~> 1.35'
 depends          'runit', '~> 1.7'
-depends          'zookeeper', '~> 6.0'
+depends          'zookeeper', '~> 8.0'
 depends          'magic', '~> 1.5'
 depends          'et_gradle', '~> 2.0'
 depends          'maven', '~> 2.2'
